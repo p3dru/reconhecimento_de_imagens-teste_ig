3b3daa6df2f168aba5cf399d71497346dd6c57da
João Pedro <61830482+p3dru@users.noreply.github.com>
2023-01-25 15:07:23 -0300

    Add files via upload

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ from screen_manager import ScreenManager
 
 def run():
     while True:
-        coordinates = ScreenManager.get_all_matches_by_image(image_to_search="teste.png")
-        for coordinate in coordinates:
-            print(coordinate)
-
+        if ScreenManager.is_app_focused(app_title_name = "instagram"):              #se o título da janela em foco, for o que queremos executar, executa a verificação
+            coordinates = ScreenManager.search_image_on_screen(image_to_search="teste.png")
+            for coordinate in coordinates:
+                print(coordinate)                               #printa as coordenadas da imagem na tela
 
 run()
--- screen_manager.py
+++ screen_manager.py
@@ -1,13 +1,21 @@
 import pyautogui
 import logging
 import cv2
+import win32gui
 
 
 class ScreenManager:
     
     @staticmethod
-    def get_all_matches_by_image(image_to_search: str):
+    def is_app_focused(app_title_name: str):                    #verifica o nome da janela
+        active_window_title = win32gui.GetWindowText(win32gui.GetForegroundWindow()).lower()    #pega o nome da janela atual e seta como minúscula
+        return app_title_name in active_window_title                #verifica com true ou false
+
+    @staticmethod                                               #serve para não precisar instanciar a classe
+    def search_image_on_screen(image_to_search: str):
         try:
-            return pyautogui.locateAllOnScreen(image=image_to_search, grayscale=True, confidence=0.9)
+            return pyautogui.locateAllOnScreen(image=image_to_search, grayscale=True, confidence=0.9) #pyautogui.locateAllOnScreen para verificar na tela inteira do computador
+            #confidence é uim parâmetro que verifica, em uma escala de 0 à 1 o nível de fidelidade da imagem
+            #grayscale deixa a imagem em tons de cinza
         except Exception as ex:
-            logging.info(f"Imagem não encontrada. Erro: {ex}")
+            logging.info(f"Imagem não encontrada. Erro: {ex}")   #informa o erro
